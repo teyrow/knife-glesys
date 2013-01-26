a04223e80121bffe64877a56ca432b0096ed6850
Simon Gate <simon@smgt.me>
2013-01-26 12:56:22 +0100

    Now you can delete several servers with one command

--- lib/chef/knife/glesys_server_delete.rb
+++ lib/chef/knife/glesys_server_delete.rb
@@ -10,7 +10,7 @@ class Chef
 
       include Knife::GlesysBase
 
-      banner "knife glesys server delete [SERVER] (options)"
+      banner "knife glesys server delete SERVER [SERVER] (options)"
 
       deps do
         require 'fog'
@@ -53,50 +53,52 @@ class Chef
       end
 
       def run
-        server_id = @name_args.first
-        server = connection.servers.get(server_id)
-        msg_pair("Server ID", server.serverid)
-        state = case server.state.to_s.downcase
-          when 'shutting-down','terminated','stopping','stopped' then ui.color(server.state, :red)
-          when 'pending' then ui.color(server.state, :yellow)
-          else ui.color(server.state, :green)
-        end
-        msg_pair("State", ui.color(state,:bold))
-        msg_pair("Hostname", server.hostname)
-        msg_pair("Description", server.description) if server.respond_to? :description # When fog supports description
-        puts "\n"
-        msg_pair("IPv4", server.iplist.select{|i| i["version"] == 4}.collect{|i| i["ipaddress"]}.join(", "))
-        msg_pair("IPv6", server.iplist.select{|i| i["version"] == 6}.collect{|i| i["ipaddress"]}.join(", "))
-        puts "\n"
-        msg_pair("CPU Cores", server.cpucores)
-        msg_pair("Memory", "#{server.memorysize} MB")
-        msg_pair("Disk", "#{server.disksize} GB")
-        puts "\n"
-        msg_pair("Template", server.templatename)
-        msg_pair("Platform", server.platform)
-        msg_pair("Datacenter", server.datacenter)
-        puts "\n"
-        msg_pair("Transfer", "#{server.transfer['usage']} of #{server.transfer['max']} #{server.transfer['unit']}")
-        msg_pair("Cost", "#{server.cost['amount']} #{server.cost['currency']} per #{server.cost['timeperiod']}")
+        @name_args.each do |server_id|
+          server = connection.servers.get(server_id)
+          puts "\n"
+          msg_pair("Server ID", server.serverid)
+          state = case server.state.to_s.downcase
+            when 'shutting-down','terminated','stopping','stopped' then ui.color(server.state, :red)
+            when 'pending' then ui.color(server.state, :yellow)
+            else ui.color(server.state, :green)
+          end
+          msg_pair("State", ui.color(state,:bold))
+          msg_pair("Hostname", server.hostname)
+          msg_pair("Description", server.description) if server.respond_to? :description # When fog supports description
+          puts "\n"
+          msg_pair("IPv4", server.iplist.select{|i| i["version"] == 4}.collect{|i| i["ipaddress"]}.join(", "))
+          msg_pair("IPv6", server.iplist.select{|i| i["version"] == 6}.collect{|i| i["ipaddress"]}.join(", "))
+          puts "\n"
+          msg_pair("CPU Cores", server.cpucores)
+          msg_pair("Memory", "#{server.memorysize} MB")
+          msg_pair("Disk", "#{server.disksize} GB")
+          puts "\n"
+          msg_pair("Template", server.templatename)
+          msg_pair("Platform", server.platform)
+          msg_pair("Datacenter", server.datacenter)
+          puts "\n"
+          msg_pair("Transfer", "#{server.transfer['usage']} of #{server.transfer['max']} #{server.transfer['unit']}")
+          msg_pair("Cost", "#{server.cost['amount']} #{server.cost['currency']} per #{server.cost['timeperiod']}")
 
-        puts "\n"
+          puts "\n"
 
-        ui.confirm("Do you wan't to delete this server?")
+          ui.confirm("Do you wan't to delete this server?")
 
-        if config[:keepip]
-          server.keepip = config[:keepip]
-        end
+          if config[:keepip]
+            server.keepip = config[:keepip]
+          end
 
-        server.destroy
+          server.destroy
 
-        ui.warn("Deleted server '#{server_id}'")
+          ui.warn("Deleted server '#{server_id}'")
 
-        if config[:purge]
-          thing_to_delete = config[:chef_node_name] || server_id
-          destroy_item(Chef::Node, thing_to_delete, "node")
-          destroy_item(Chef::ApiClient, thing_to_delete, "client")
-        else
-          ui.warn("Corresponding node and client for the #{server_id} server were not deleted and remain registered with the Chef Server")
+          if config[:purge]
+            thing_to_delete = config[:chef_node_name] || server_id
+            destroy_item(Chef::Node, thing_to_delete, "node")
+            destroy_item(Chef::ApiClient, thing_to_delete, "client")
+          else
+            ui.warn("Corresponding node and client for the #{server_id} server were not deleted and remain registered with the Chef Server")
+          end
         end
 
       rescue NoMethodError
