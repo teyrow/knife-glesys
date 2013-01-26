fcc9f8fd451cbc3c137f7b62e1b44b1d3ceafce6
Simon Gate <simon@smgt.me>
2013-01-26 13:03:04 +0100

    Update template list to only show existing data

--- lib/chef/knife/glesys_template_list.rb
+++ lib/chef/knife/glesys_template_list.rb
@@ -16,24 +16,16 @@ class Chef
       def run
 
         template_list = [
-          ui.color('ID', :bold),
           ui.color('Platform', :bold),
-          ui.color('Name', :bold),
-          ui.color('OS', :bold),
-          ui.color('Min Memory', :bold),
-          ui.color('Min Disk', :bold),
+          ui.color('Template Name', :bold),
         ]
 
         connection.templates.sort_by(&:templateid).each do |template|
-          template_list << template.templateid.to_s
           template_list << template.platform
           template_list << template.name
-          template_list << template.os
-          template_list << template.min_mem_size
-          template_list << template.min_disk_size
         end
 
-        puts ui.list(template_list, :columns_across, 6)
+        puts ui.list(template_list, :columns_across, 2)
       end
     end
   end
