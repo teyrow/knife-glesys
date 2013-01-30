7184ab4e8b6473f62e6e136ce8f8d30be9a8cc8d
Simon Gate <simon@smgt.me>
2013-01-30 21:55:50 +0100

    Update template list to work with fog HEAD

--- lib/chef/knife/glesys_template_list.rb
+++ lib/chef/knife/glesys_template_list.rb
@@ -17,15 +17,22 @@ class Chef
 
         template_list = [
           ui.color('Platform', :bold),
-          ui.color('Template Name', :bold),
+          ui.color('Name', :bold),
+          ui.color('Operating System', :bold),
+          ui.color('Min Memory Size', :bold),
+          ui.color('Min Disk Size', :bold),
+
         ]
 
-        connection.templates.sort_by(&:templateid).each do |template|
+        connection.templates.sort_by(&:platform).each do |template|
           template_list << template.platform
           template_list << template.name
+          template_list << template.operating_system
+          template_list << "#{template.minimum_memory_size} mb"
+          template_list << "#{template.minimum_disk_size} gb"
         end
 
-        puts ui.list(template_list, :columns_across, 2)
+        puts ui.list(template_list, :columns_across, 5)
       end
     end
   end
