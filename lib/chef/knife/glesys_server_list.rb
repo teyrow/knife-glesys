a256b94af60e52730a004a2b7435d56ab5875f2e
Simon Gate <simon@smgt.me>
2013-02-04 21:09:58 +0100

    Remove server inspect

--- lib/chef/knife/glesys_server_list.rb
+++ lib/chef/knife/glesys_server_list.rb
@@ -16,7 +16,6 @@ class Chef
 
       def run
 
-
         server_list = [
           ui.color('ID', :bold),
           ui.color("Hostname", :bold),
@@ -29,18 +28,6 @@ class Chef
           server_list << server.hostname
           server_list << server.platform
           server_list << server.datacenter
-          # server_list << begin
-          #   state = server.state.to_s.downcase
-          #   case state
-          #     when 'shutting-down','terminated','stopping','stopped'
-          #     ui.color(state, :red)
-          #     when 'pending'
-          #     ui.color(state, :yellow)
-          #     else
-          #     ui.color(state, :green)
-          #   end
-          # end
-          puts server.inspect
         end
 
         puts ui.list(server_list, :columns_across, 4)
